68f7644bfc3d29e96b35c9149d6a14d71edbcc6e
Musti <safi.najjar1@gmail.com>
2025-01-27 15:05:02 +0100

    fetch data in app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import ShopCart from './components/ShopCart.vue'
 import ShopProducts from './components/ShopProducts.vue'
+import { ref } from 'vue'
+
+const products = ref([])
+
+// fetch products from data.json
+fetch('/data.json')
+  .then((res) => {
+    return res.json()
+  })
+  .then((data) => {
+    products.value = data
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 </script>
 
 <template>
-  <main class="container flex mx-auto justify-between gap-12 py-12">
-    <ShopProducts />
+  <main class="container md:flex md:mx-auto md:justify-between gap-12 py-12 px-10">
+    <ShopProducts :products="products" />
     <ShopCart />
   </main>
 </template>
