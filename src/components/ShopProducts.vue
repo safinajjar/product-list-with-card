aa36cb8fc4b0f2d04d672a0d793a44f271c9f3df
Musti <safi.najjar1@gmail.com>
2025-01-30 10:40:07 +0100

    formating

--- src/components/ShopProducts.vue
+++ src/components/ShopProducts.vue
@@ -21,7 +21,7 @@ defineProps<{
 
 <template>
   <section class="products grow">
-    <h1 class="text-5xl font-bold mb-8">Desserts</h1>
+    <h1 class="mb-8 text-5xl font-bold">Desserts</h1>
 
     <ul class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
       <ShopProduct :product="product" v-for="product in products" :key="product.id" />
